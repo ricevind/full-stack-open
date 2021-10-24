0b0ef0b962605e67f38237369674bd50aeccac54
Ricevind <ricevind@gmail.com>
2021-10-24 17:54:51 +0200

    part1:unicafe:1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -3,10 +3,17 @@ import React, { useState } from "react";
 const addOneTo = (counterSetter) => () =>
   counterSetter((counter) => counter + 1);
 
+const toPrecision = (value) => value.toPrecision(4);
+const toPercentage = (value) => `${toPrecision(value * 100)} %`;
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+
+  const all = good + neutral + bad;
+  const average = toPrecision(all && (good - bad) / all);
+  const positive = toPercentage(all && good / all);
 
   return (
     <div>
@@ -22,6 +29,12 @@ const App = () => {
         <p>good: {good}</p>
         <p>neutral: {neutral}</p>
         <p>bad: {bad}</p>
+      </div>
+
+      <div>
+        <p>all: {all}</p>
+        <p>average: {average}</p>
+        <p>positive: {positive}</p>
       </div>
     </div>
   );
