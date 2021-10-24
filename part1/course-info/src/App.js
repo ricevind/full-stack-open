0f27d17bbaf74fda55f92f35316e3678da72a925
Ricevind <ricevind@gmail.com>
2021-10-24 12:46:05 +0200

    part1:course-info:1.5

--- part1/course-info/src/App.js
+++ part1/course-info/src/App.js
@@ -1,27 +1,29 @@
 import { Total, Header, Content } from "./components";
 
 export const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
 
   return (
     <div>
-      <Header title={course} />
-      <Content parts={parts}></Content>
-      <Total parts={parts} />
+      <Header title={course.name} />
+      <Content parts={course.parts}></Content>
+      <Total parts={course.parts} />
     </div>
   );
 };
