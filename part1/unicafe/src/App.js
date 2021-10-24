03bc41531749e8a04a6a43dc6bb2b02a64b9fcc1
Ricevind <ricevind@gmail.com>
2021-10-24 18:13:31 +0200

    part1:unicafe:1.8-1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,33 +11,56 @@ const App = () => {
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const all = good + neutral + bad;
-  const average = toPrecision(all && (good - bad) / all);
-  const positive = toPercentage(all && good / all);
-
   return (
-    <div>
+    <div style={{ padding: "0 1rem" }}>
       <h1>Gib fitBack</h1>
       <div>
-        <button onClick={addOneTo(setGood)}>good</button>
-        <button onClick={addOneTo(setNeutral)}>neutral</button>
-        <button onClick={addOneTo(setBad)}>bad</button>
+        <Button onClickHandle={addOneTo(setGood)}>good</Button>
+        <Button onClickHandle={addOneTo(setNeutral)}>neutral</Button>
+        <Button onClickHandle={addOneTo(setBad)}>bad</Button>
       </div>
 
-      <h1>Statistics</h1>
-      <div>
-        <p>good: {good}</p>
-        <p>neutral: {neutral}</p>
-        <p>bad: {bad}</p>
-      </div>
-
-      <div>
-        <p>all: {all}</p>
-        <p>average: {average}</p>
-        <p>positive: {positive}</p>
-      </div>
+      <Statistics {...{ good, bad, neutral }}></Statistics>
     </div>
   );
 };
 
 export default App;
+
+const Button = ({ onClickHandle, children }) => (
+  <button onClick={onClickHandle}>{children}</button>
+);
+
+const StatisticsLine = ({ text, value }) => (
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = toPrecision(all && (good - bad) / all);
+  const positive = toPercentage(all && good / all);
+
+  if (all === 0) {
+    return <p>No fitBack gibed</p>;
+  }
+
+  return (
+    <>
+      <h1>Statistics</h1>
+      <table>
+        <tbody>
+          <StatisticsLine text="good" value={good} />
+          <StatisticsLine text="neutral" value={neutral} />
+          <StatisticsLine text="bad" value={bad} />
+
+          <StatisticsLine text="all" value={all} />
+          <StatisticsLine text="average" value={average} />
+          <StatisticsLine text="positive" value={positive} />
+        </tbody>
+      </table>
+    </>
+  );
+};
