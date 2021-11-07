2fa344631c53e086250f939335beb71e21bf88a0
Ricevind <ricevind@gmail.com>
2021-11-07 15:11:55 +0100

    part2:phonebook:2.15-2.20

--- part2/phonebook/src/App.tsx
+++ part2/phonebook/src/App.tsx
@@ -2,11 +2,13 @@ import './App.css';
 
 import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 
-type Person = {
-  name: string;
-};
-
-type Persons = Person[];
+import {
+  Person,
+  PersonCreationParams,
+  Persons,
+  PersonWithTemporaryId,
+} from './entities/person.model';
+import { personsService } from './entities/person.service';
 
 function useIncludes<Elem>(arr: Elem[]): [(elem: Elem) => boolean] {
   const includes = (elem: Elem) => arr.includes(elem);
@@ -18,20 +20,7 @@ function usePersons() {
   const [persons, setPersons] = useState<Persons>([]);
 
   useEffect(() => {
-    const url = 'http://localhost:3001/persons';
-
-    fetch(url)
-      .then((response) => response.json())
-      .then((responseBody) => {
-        const arePersons = (rb: unknown): rb is Persons =>
-          Array.isArray(responseBody) && 'name' in responseBody[0];
-
-        if (arePersons(responseBody)) {
-          return responseBody;
-        }
-        throw new Error('API did not returned compatible response');
-      })
-      .then((responseBody) => setPersons(responseBody));
+    personsService.getPersons().then((responseBody) => setPersons(responseBody));
   }, []);
 
   return [persons, setPersons] as const;
@@ -39,25 +28,99 @@ function usePersons() {
 
 function App(): JSX.Element {
   const [persons, setPersons] = usePersons();
-  const [newName, setName] = useState<string>('');
+  const [newPerson, setNewPerson] = useState<PersonCreationParams>({
+    name: '',
+    number: '',
+  });
   const [nameAlreadyExist] = useIncludes(persons.map(({ name }) => name));
+  const [successNotification, setSuccessNotification] = useState<null | string>(null);
+  const [errorNotification, setErrorNotification] = useState<null | string>(null);
+
+  const displaySuccessMessage = (message: string) => {
+    setSuccessNotification(message);
+
+    setTimeout(() => setSuccessNotification(null), 2000);
+  };
+
+  const displayErrorMessage = (message: string) => {
+    setErrorNotification(message);
+
+    setTimeout(() => setErrorNotification(null), 2000);
+  };
 
   const onAddPerson = (e: FormEvent) => {
     e.preventDefault();
 
-    if (nameAlreadyExist(newName)) {
-      alert(`${newName} was already defined`);
-      return;
+    if (nameAlreadyExist(newPerson.name)) {
+      const shouldReplace = confirm(
+        `${newPerson.name} is already in teh phone book, replace the old number with a new one ?`,
+      );
+
+      if (!shouldReplace) {
+        return;
+      }
+
+      const person = persons.find((p) => p.name === newPerson.name);
+
+      if (person && Person.isCreatedPerson(person)) {
+        const updatedPerson = { ...person, ...newPerson };
+
+        setPersons(Person.updatePerson(persons, updatedPerson));
+
+        personsService
+          .updatePerson(updatedPerson)
+          .then(() => setNewPerson({ name: '', number: '' }))
+          .then(() => displaySuccessMessage(`${updatedPerson.name} has been updated`))
+          .catch(() => {
+            return setPersons(persons);
+          });
+
+        return;
+      }
     }
 
-    const newPerson: Person = { name: newName };
-    setPersons(persons.concat(newPerson));
-    setName('');
+    const temporaryId = Math.random() * 1000 + '';
+    const newPersonWithTemporaryId: PersonWithTemporaryId = { ...newPerson, temporaryId };
+    setPersons(persons.concat(newPersonWithTemporaryId));
+    setNewPerson({ name: '', number: '' });
+
+    personsService
+      .addPerson(newPerson)
+      .then((data) =>
+        setPersons((persons) =>
+          Person.removePersonById(persons, temporaryId).concat(data),
+        ),
+      )
+      .then(() => displaySuccessMessage(`${newPerson.name} has been created`));
+  };
+
+  const onDeletePerson = (person: Person) => {
+    const canDelete = confirm(`Should ${person.name} be deleted ?`);
+    const personServerId = Person.getPersonServerId(person);
+
+    if (canDelete) {
+      setPersons(Person.removePerson(persons, person));
+
+      personsService.deletePerson(personServerId).catch((e) => {
+        console.log(e);
+        if (e === 404) {
+          displayErrorMessage(`${person.name} was already removed from the server`);
+          return;
+        }
+
+        setPersons(persons);
+      });
+    }
   };
 
   const onNewNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const newName = e.target.value;
-    setName(newName);
+    const name = e.target.value;
+    setNewPerson({ ...newPerson, name });
+  };
+
+  const onNewNumberChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const number = e.target.value;
+    setNewPerson({ ...newPerson, number });
   };
 
   return (
@@ -65,15 +128,32 @@ function App(): JSX.Element {
       <h2>Phonebook</h2>
       <form onSubmit={onAddPerson}>
         <div>
-          name: <input value={newName} onChange={onNewNameChange} />
+          name: <input value={newPerson.name} onChange={onNewNameChange} />
+        </div>
+        <div>
+          number: <input value={newPerson.number} onChange={onNewNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+
+      {successNotification && (
+        <div className="notification notification_success">{successNotification}</div>
+      )}
+      {errorNotification && (
+        <div className="notification notification_error">{errorNotification}</div>
+      )}
+
       {persons.map((person) => (
-        <div key={person.name}>{person.name}</div>
+        <div key={person.name} style={{ display: 'flex' }}>
+          <div>{person.name}</div>
+          <div>{person.number}</div>
+          <button onClick={() => onDeletePerson(person)} type="button">
+            Delete
+          </button>
+        </div>
       ))}
     </div>
   );
